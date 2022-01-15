2e99890c679a14f5830a1f472740b7dfbc5bdaab
JKal24 <jkalirai2499@gmail.com>
2022-01-14 21:25:18 -0500

    Created rotating image carousel with antd component, images can be put on the dynamic array

--- humans-proj/src/pages/about-page/AboutPage.js
+++ humans-proj/src/pages/about-page/AboutPage.js
@@ -1,36 +1,65 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import "./AboutPage.css";
+import "antd/dist/antd.css";
 import Footer from "../components/Footer/Footer";
 import photo from '../../../static/images/about-us-claybanks.png';
 import photo2 from '../../../static/images/Rectangle.png';
+import { Carousel } from "antd";
 
 export default function AboutPage() {
+    // Put images for carousel in this array
+    const images = [photo, photo, photo];
+    const carousel = React.createRef();
+
+    const nextImage = () => {
+        carousel.current.next();
+    }
+
+    const prevImage = () => {
+        carousel.current.prev();
+    }
+
     return (
         <div id="about-page">
             <Header />
-            <section className= "image-section">
-                <button className = "arrow-button"><h1 className = "small-arrow">&lt;</h1></button>
-                <button className = "arrow-button"><h1 className = "big-arrow">&lt;</h1></button>
+            <section className="image-section">
+                <div className="image-slider">
 
-                <div className = "image-overlap">
-                <img className="changeable-image" src={photo} alt="" />
-                <img className="box" src={photo2} alt="" />
+                    <button className = "arrow-button"><h1 className = "small-arrow">&lt;</h1></button>
+                    <button className = "arrow-button"><h1 className = "big-arrow">&lt;</h1></button>
+
+                    <div className="image-overlap">
+                        <Carousel ref={carousel} dots={false} autoplay>
+                            {
+                                images.map((imageSrc, index) => {
+                                    return (
+                                        <div className="image-overlap" key={index}>
+                                            <img className="changeable-image" src={imageSrc} alt="" />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </Carousel>
+                        <img className="box" src={photo2} alt="" />
+                    </div>
+
+                    <button className = "arrow-button"><h1 className = "big-arrow">&gt;</h1></button>
+                    <button className = "arrow-button"><h1 className = "small-arrow">&gt;</h1></button>
+
                 </div>
-                <button className = "arrow-button"><h1 className = "big-arrow">&gt;</h1></button>
-                <button className = "arrow-button"><h1 className = "small-arrow">&gt;</h1></button>
             </section>
 
-            <section className = "questions">
-                <div className= "first-row">
-                    <div className ="faq">
+            <section className="questions">
+                <div className="first-row">
+                    <div className="faq">
                         <h2>What we do?</h2>
                         <ul>
                             <li>We help spread the word for good</li>
                             <li>Created to spread awareness</li>
                         </ul>
                     </div>
-                    <div className ="faq">
+                    <div className="faq">
                         <h2>More About Software For Love</h2>
                         <ul>
                             <li>We help spread the word for good</li>
@@ -40,25 +69,25 @@ export default function AboutPage() {
                         </ul>
                     </div>
                 </div>
-                <div className= "second-row">
-                <div className ="faq">
-                    <h2>Why we do it?</h2>
-                    <ul>
-                        <li>We help spread the word for good</li>
-                        <li>Created to spread awareness</li>
-                        <li>We help spread the word for good</li>
-                        <li>Created to spread awareness</li>
-                    </ul>
-                </div>
-                <div className ="faq">
-                    <h2>Why we do it?</h2>
-                    <ul>
-                        <li>We help spread the word for good</li>
-                        <li>Created to spread awareness</li>
-                        <li>We help spread the word for good</li>
-                        <li>Created to spread awareness</li>
-                    </ul>
-                </div>
+                <div className="second-row">
+                    <div className="faq">
+                        <h2>Why we do it?</h2>
+                        <ul>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                        </ul>
+                    </div>
+                    <div className="faq">
+                        <h2>Why we do it?</h2>
+                        <ul>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                        </ul>
+                    </div>
                 </div>
 
             </section>

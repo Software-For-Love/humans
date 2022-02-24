42ee9c7a6ec1c6b72cda3995033db3c3876bffa9
Jasmine Kokkat <jasminekokkat@Jasmines-MacBook-Pro.local>
2022-02-24 12:05:45 -0500

    added YAML file and integrated it in JS file

--- humans-proj/src/pages/feature-page/currently-featured.js
+++ humans-proj/src/pages/feature-page/currently-featured.js
@@ -7,9 +7,13 @@ import insta from '../../../static/images/insta_icon.png';
 import facebook from '../../../static/images/facebook_icon.png';
 import twitter from '../../../static/images/twitter_icon.png';
 import internet from '../../../static/images/internet_icon.png';
-
+//import getData from '../../utils/getData';
+import featured_data from "../../data/currently-featured.yaml";
+import _ from "lodash";
 
 export default function CurrFeaturePage() {
+  //  let featured_data = getData(
+     //   this.props.pageContext.site.data);
     return (
         <div id="curr-feature-page">
             <Header />
@@ -20,29 +24,37 @@ export default function CurrFeaturePage() {
                 <div id="featured-icon">
                     <div className="square" id="square1"></div>
                     <div className="square" id="square2"></div>
-                    <img src={photo} id="main_pic" className="photo-for-mainpage" alt="Person" />
+                    <img src={featured_data.photo} id="main_pic" className="photo-for-mainpage" alt={featured_data.photo_alt} />
                 </div>
 
                 <div id="featured-info">
-                    <h1 id="main_text">Name Here</h1>
+                    <h1 id="main_text">{featured_data.first_name}{featured_data.last_name}</h1>
                     <p>
                         <button id="all_medias">
+                            <a href={featured_data.socials.instagram}>
                             <img src={insta} id="insta_pic" className="photo-for-mainpage" alt="Instagram" />&nbsp;
+                            </a>
+                            <a href ={featured_data.socials.facebook}>
                             <img src={facebook} id="facebook_pic" className="photo-for-mainpage" alt="Facebook" />
+                            </a>
+                            <a href ={featured_data.socials.twitter}>
                             <img src={twitter} id="twitter_pic" className="photo-for-mainpage" alt="Twitter" />
+                            </a>
+                            <a href ={featured_data.socials.internet}>
                             <img src={internet} id="internet_pic" className="photo-for-mainpage" alt="Internet" />
+                            </a>
                         </button>
                     </p>
-                    <p id="born">Key Fact 1 Here: Dolor sit amet</p>
-                    <p id="worth">Key Fact 2 Here: Adipiscing elit</p>
-                    <p id="partner">Key Fact 3 Here: Eiusmod tempor incididunt ut labore et</p>
-                    <p id="LG">Type of Activist Here</p>
+                    <p id="born">{featured_data.facts.birth}</p>
+                    <p id="worth">{featured_data.facts.worth}</p>
+                    <p id="partner">{featured_data.facts.partner}</p>
+                    <p id="LG">{featured_data.position}</p>
                 </div>
             </div>
 
             <div id="full-description">
-                    <p id="para1" className="para">Full Description Here ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    <p id="para2" className="para">Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at.</p>
+                    <p id="para1" className="para">{featured_data.description} </p>
+                    <p id="para2" className="para">{featured_data.description2}</p>
                 </div>
             <Footer />
         </div>

fba3501ea118d031538700f02aa17fd766364f65
Zohair Ahmed <Zohair@Zohairs-MBP.hitronhub.home>
2022-04-02 15:46:36 -0400

    (#46) - Add a filter for the all featured page
    The information can be found in the all-featured.yaml file. Also,
    a Card component was created to allow for readability and easier
    editing.

---
+++ humans-proj/src/pages/components/Card/Card.js
@@ -0,0 +1,90 @@
+import React from "react"
+import "./Card.css"
+import { SocialIcon } from 'react-social-icons';
+import { Link } from 'gatsby';
+import temp_file from "../../../data/currently-featured.yaml";
+
+export default class Card extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { src: null }
+    }
+
+    componentDidMount() { this.loadImage(this.props.person.img) }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.person.img !== this.props.person.img) {
+            this.loadImage(this.props.person.img)
+        }
+    }
+
+    loadImage(name) {
+        import(`../../../../static/images/${name}`)
+            .then(image => {
+                this.setState({ src: image.default })
+            })
+    }
+
+
+    render() {
+        return (
+            <div id="card-wrapper">
+                <div id="card">
+
+                    {/* CARD IMAGE */}
+                    <div id="card-img-div">
+                        <img id="card-img" src={this.state.src} alt={this.props.person.photo_alt} />
+                    </div>
+
+                    {/* CARD NAME */}
+                    <div id="card-name-div-wrapper">
+                        <div id="card-name-div">
+                            <p id="card-name">{this.props.person.photo_alt} </p>
+                        </div>
+                    </div>
+
+
+                    {/* CARD ON-HOVER DESCRIPTION */}
+                    <div id="middle">
+                        <Link to="/feature-page/currently-featured/" state={{ file: temp_file }} style={{ textDecoration: "none", color: "white" }}>
+                            <div id="overlay-wrapper">
+
+                                <p className="overlay-text" id="overlay-name"> {this.props.person.first_name + " " + this.props.person.last_name} </p>
+                                <p className="overlay-text" id="overlay-job">{this.props.person.position}</p>
+                                <div id="overlay-desc-wrapper">
+                                    <p className="overlay-text" id="overlay-desc">{this.props.person.description}</p>
+                                </div>
+
+
+
+                                <div id="card-links-wrapper">
+                                    <div id="card-links">
+                                        {
+                                            !this.props.person.socials ? <></> :
+
+                                                this.props.person.socials.map(link => {
+                                                    return (
+                                                        <div id="card-icon-list">
+
+                                                            <SocialIcon id="card-icon" url={link}
+                                                                fgColor="black"
+                                                                bgColor="white"
+                                                                target={"_blank"}
+                                                                style={{ width: "40px", height: "40px", margin: "10px" }}
+                                                            />
+                                                        </div>
+                                                    )
+                                                })
+                                        }
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- humans-proj/src/pages/feature-page/feature-page.js
+++ humans-proj/src/pages/feature-page/feature-page.js
@@ -1,102 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from 'gatsby';
 import Header from "../components/Header/Header";
 import "./feature-page.css";
 import Footer from "../components/Footer/Footer";
-import img1 from "../../../static/images/img1.png";
-import img2 from "../../../static/images/img2.png";
-import img3 from "../../../static/images/img3.png";
-import img4 from "../../../static/images/img 4.png";
 import rightArrows from '../../../static/images/right-arrows.png';
-import insta from '../../../static/images/insta_icon.png';
-import facebook from '../../../static/images/facebook_icon.png';
-import twitter from '../../../static/images/twitter_icon.png';
-import internet from '../../../static/images/internet_icon.png';
+import Card from "../components/Card/Card"
 
 // Temporary file to use redirects for currently featured page
-import temp_file from "../../data/currently-featured.yaml";
+import yamdata from "../../data/all-features-page/all-featured.yaml"
 
-/**
- * Featured data should be stored here based on the month and year that it matches to.
- * 
- * Every month should have an array containing an object that represents a featured person
- * which has an associated name, last name, title, description and image.
- * 
- * When integrating the yaml file, you can replace the object below with a yaml file like this
- * 
- * "yaml_date1": [
- *      yaml_section1,
- *      yaml_section2,
- *      yaml_section3
- * ], "yaml_date2": [...
- * 
- * The yaml file should ideally follow a similar structure where each person is mapped to a specific date
- * 
- * Yaml files may have different names for their element mappings, you can update the featured people 
- * cards below to reflect the mapped names
- */
+export default function FeaturePage(props) {
 
-const featuredData =
-{
-    "2021 August": [
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img1
-        },
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img2
-        },
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img3
-        }
-    ],
-    "2021 July": [
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img4
-        },
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img1
-        },
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img2
-        },
-        {
-            name: "Name",
-            lastName: "LastName",
-            title: "Journalist",
-            desc: "We look forward to the time when the power to love will replace the love of power. Then will our world know the blessing of peace.",
-            img: img3
-        }
-    ]
-}
+    const featuredData = props.location.state && props.location.state.file ? props.location.state.file : yamdata;
 
-export default function FeaturePage() {
+    // filters featured people
+    const [searchTerm, setSearchTerm] = useState('')
 
     // Keeps track of the featured people visible on screen
-    const [startDateIndex, setStartDateIndex] = useState(0);
+    const [startActivistIndex, setStartActivistIndex] = useState(0);
     const [startPeopleIndex, setStartPeopleIndex] = useState(0);
 
     // These variables handle navigation between people
@@ -120,7 +40,6 @@ export default function FeaturePage() {
 
         for (const featuredPeople in featuredData) {
             const len = featuredData[featuredPeople].length;
-
             currentLengths.push(len);
         }
 
@@ -131,138 +50,107 @@ export default function FeaturePage() {
     }, [])
 
     function handleCarousel() {
-
         // Increments the beginning index
-        const len = lengths[startDateIndex];
+        const len = lengths[startActivistIndex];
         const newStartPeopleIndex = startPeopleIndex + 1;
 
         if (newStartPeopleIndex >= len) {
-
-            setStartDateIndex((startDateIndex + 1) % lengths.length);
+            setStartActivistIndex((startActivistIndex + 1) % lengths.length);
             setStartPeopleIndex(0);
-
         } else {
-
             setStartPeopleIndex(newStartPeopleIndex);
-
         }
     }
 
     return (
         <div id="feature-page">
             <Header />
+
             <div class="feature-container">
                 <div class="square" id="afbar"></div>
+
                 <div class="feature-search">
                     <div class="dropdown">
                         <button id="months">Month ↓</button>
                         <div class="dropdown_content">
-                            <p>January</p>
-                            <p>Feburary</p>
-                            <p>March</p>
-                            <p>April</p>
-                            <p>May</p>
-                            <p>June</p>
-                            <p>July</p>
-                            <p>August</p>
-                            <p>September</p>
-                            <p>October</p>
-                            <p>November</p>
-                            <p>December</p>
+                            {Object.keys(featuredData).map(item => <p> {item} </p>)}
                         </div>
                     </div>
-                    <input id="search_bar" type="text" placeholder="Search name, stories..."></input>
+                    <input id="search_bar" type="text" placeholder="Search name, stories..." onChange={event => { setSearchTerm(event.target.value) }}></input>
                 </div>
 
-                <div class="featured-contents">
-                    {
-                        (() => {
-                            renderSize.current = size;
-                            const elements = [];
-                            let repeat = false;
+                <div id="featured-contents-wrapper">
+                    <div class="arrow-container">
+                        <button type="button" id="arrow-right" onClick={handleCarousel}><img src={rightArrows} alt="arrows" /></button>
+                    </div>
 
-                            while (renderSize.current > 0) {
-                                elements.push(Object.keys(featuredData).map((date, dateIndex) => {
-                                    const objectData = featuredData[date];
-                                    // Map all elements in the array to the specified date
-                                    if (renderSize.current <= 0 || (dateIndex < startDateIndex && !repeat)) return <></>;
-                                    return (
-                                        <div class="featured-date">
+                    <div id="featured-list-wrapper">
+                        <div id="featured-contents">
+                            {
+                                (() => {
+                                    renderSize.current = size;
+                                    const elements = [];
+                                    let repeat = false;
 
-                                            <div id="date-container">
-                                                <h2 id="m2">{date}</h2>
-                                            </div>
-                                            
-                                            <div class="featured-people">
-                                                {
-                                                    objectData.map((person, personIndex) => {
-                                                        if (renderSize.current <= 0 || 
-                                                            (dateIndex === startDateIndex && !repeat && personIndex < startPeopleIndex)) return <></>;
-                                                        // Each array element is represented individually
-                                                        renderSize.current--;
-                                                        return (
+                                    while (renderSize.current > 0) {
+                                        elements.push(Object.keys(featuredData).map((id, ActivistIndex) => {
+                                            const objectData = featuredData[id];
+                                            // Map all elements in the array to the specified date
+                                            if (renderSize.current <= 0 || (ActivistIndex < startActivistIndex && !repeat)) return <></>;
+                                            return (
+                                                <>
+                                                    <div id="featured-month-list">
+                                                        {/* <p id="featured-date2">{id}</p> */}
 
-                                                            // Each featured person card should reference a yaml file, read the comments above near the featuredData
-                                                            // object as to how each yaml file should be handled
+                                                        <div id="featured-month-people">
+                                                            {
+                                                                objectData.filter((val) => {
+                                                                    if (searchTerm == "" || val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                                                        return val
+                                                                    }
+                                                                }).map((person, personIndex) => {
+                                                                    if (renderSize.current <= 0 ||
+                                                                        (ActivistIndex === startActivistIndex && !repeat && personIndex < startPeopleIndex)) return <></>;
+                                                                    // Each array element is represented individually
+                                                                    renderSize.current--;
+                                                                    return (
+                                                                        <div style={{display:"block"}}>
+                                                                            <p id="featured-date2">{person.date}</p>
+                                                                            <div id="featured-person">
+                                                                                <Card id="featured-card" person={person} />
+                                                                            </div>
+                                                                        </div>
+                                                                    )
+                                                                })
+                                                            }
+                                                        </div>
+                                                    </div>
 
-                                                            // This 'file' props being passed through the Link should be updated when yaml files are integrated.
-                                                            // The 'file' should reference the featured person's yaml file.
-
-                                                            <div class="pic" key={personIndex}>
-
-                                                                {
-                                                                    // The file that is being sent in the currently featured page should represent a similar structure
-                                                                    // to one of the person sections in the yaml file, under a date
-                                                                }
-
-                                                                <Link to="/feature-page/currently-featured/" state={{ file : temp_file }}>
-
-                                                                <button class="person-btn"><img id="img" src={person.img} alt="person" /></button>
-                                                                <div class="square" id="namerec"></div>
-                                                                <div id="name">{person.name} {person.lastName}</div>
-                                                                <div class="picdet">
-                                                                    <div class="square" id="details"></div>
-                                                                    <p id="pichead">{person.name + " " + person.lastName}</p>
-                                                                    <div class="picdes">
-                                                                        <p>{person.title}</p>
-                                                                        <br></br>
-                                                                        <p>{person.desc}</p>
-                                                                        <p><button id="featured_medias">
-                                                                            <img src={insta} id="insta_pic" className="photo-for-mainpage" alt="insta" />&nbsp;
-                                                                            <img src={facebook} id="facebook_pic" className="photo-for-mainpage" alt="facebook" />
-                                                                            <img src={twitter} id="twitter_pic" className="photo-for-mainpage" alt="twitter" />
-                                                                            <img src={internet} id="internet_pic" className="photo-for-mainpage" alt="internet" />
-                                                                        </button></p>
-                                                                    </div>
-                                                                </div>
-                                                                </Link>
-                                                            </div>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        </div>
-                                    )
-                                }))
-                                repeat = true;
+                                                </>
+                                            )
+                                        }))
+                                        repeat = true;
+                                    }
+                                    return elements;
+                                })()
                             }
-                            return elements;
-                        })()
-                    }
+                        </div>
+                    </div>
 
                     <div class="arrow-container">
                         <button type="button" id="arrow" onClick={handleCarousel}><img src={rightArrows} alt="arrows" /></button>
                     </div>
-
                 </div>
+
                 <div class="square" id="nominatebar">
                     <div class="nominatebartxt">
                         <p> Didn't see your favourite human rights activist or social groups?</p>
                         <p>Write us a recommendation!</p>
-                        <button type="button" id="nominate_button" onClick={() => {window.location.replace('/nominate-page/nominate/')}}>Nominate</button>
+                        <button type="button" id="nominate_button" onClick={() => { window.location.replace('/nominate-page/nominate/') }}>Nominate</button>
                     </div>
                 </div>
             </div>
+
             <Footer />
         </div>
     )

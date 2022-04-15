e2f73a2360bc4121f5506af4eddb34c3c845b355
JKal24 <jkalirai2499@gmail.com>
2022-04-15 19:26:48 -0400

    Added card format to the text sections

--- humans-proj/src/pages/about-page/AboutPage.js
+++ humans-proj/src/pages/about-page/AboutPage.js
@@ -68,14 +68,14 @@ export default function AboutPage() {
             <section className="questions">
                 <div className="first-row">
                     <div className="faq">
-                        <h2>What is Humans?</h2>
+                        <h2 className="faqTitle">What is Humans?</h2>
                         <ul>
                             <p className="info">Humans is a project that was established through Software For Love (SFL), a not-for-profit organization that provides software solutions. Humans is an internal project that would spotlight activists within our respective communities. Each activist on this website is someone who goes out of their way to bring about positive change in our society. Humans gives activist a platform while also demonstrating our design and software development skills!</p>
                             
                         </ul>
                     </div>
                     <div className="faq">
-                        <h2>About Software For Love</h2>
+                        <h2 className="faqTitle">About Software For Love</h2>
                         <ul>
                             <p className="info">Software For Love is a not for profit organization that provides software solutions to other nonprofits and charities in order to increase the social impact of these organizations.</p>
                         </ul>
@@ -83,13 +83,13 @@ export default function AboutPage() {
                 </div>
                 <div className="second-row">
                     <div className="faq">
-                        <h2>Why we do it?</h2>
+                        <h2 className="faqTitle">Why we do it?</h2>
                         <ul>
                         <p className="info">We are passionate about the intersection of technology and activism and this project is an ideal way to execute it. With the aim “spreading love through software” we aim to use our expert knowledge in UI/UX design, software development, and more, to help smaller organizations increase their digital image in an increasingly digital world.</p>
                         </ul>
                     </div>
                     <div className="faq">
-                        <h2>What can I do?</h2>
+                        <h2 className="faqTitle">What can I do?</h2>
                         <ul>
                             <p className="info">If you have an activist that you believe deserves this spotlight, fill out our <a href='/nominate-page/nominate/'>nomination form!</a></p>
 
